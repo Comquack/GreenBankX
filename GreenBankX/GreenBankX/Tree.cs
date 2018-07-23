54e767d3031800ba68644c9837ff0ca146309294
BenJamin Wildie (Comquack) <benwildie@hotmail.com>
2018-07-23 21:53:34 +1000

    Added tree

---
+++ GreenBankX/GreenBankX/Tree.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreenBankX
+{
+    public class Tree
+    {
+        private double diameter;
+        private double merchHeight;
+        private int id;
+        // constructor
+        public Tree(float diam, float merch, int ID )
+        {
+            diameter = diam;
+            merchHeight = merch;
+            id = ID;
+        }
+
+        public double GetDia()
+        { return this.diameter;
+        }
+        public double Merch => this.merchHeight;
+        public int ID => this.id;
+        public string GetCSV()
+        {
+            String result = id + "," + diameter.ToString() + "," + merchHeight.ToString() + ";";
+            return result;
+        }
+
+    }
+}
