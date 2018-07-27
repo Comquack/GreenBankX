5fab46b2b682dc12b0785d9cba5fdbfd60c97028
BenJamin Wildie (Comquack) <benwildie@hotmail.com>
2018-07-27 23:40:34 +1000

    label text editing

--- GreenBankX/GreenBankX/MenuPage.xaml.cs
+++ GreenBankX/GreenBankX/MenuPage.xaml.cs
@@ -12,9 +12,20 @@ namespace GreenBankX
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class MenuPage : ContentPage
 	{
-		public MenuPage ()
+        Tree tree1;
+        Plot plot1;
+        Calculator calc = new Calculator();
+        double[,] result;
+        public MenuPage ()
 		{
 			InitializeComponent ();
-		}
+            tree1 = new Tree(100, 10, 1);
+            plot1 = new Plot("Ethl");
+            plot1.AddTree(tree1);
+            result = calc.Calc(100, 10);
+            Lbl1.Text =result[0,0].ToString();
+
+
+        }
 	}
 }
